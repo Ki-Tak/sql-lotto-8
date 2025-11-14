5c3127b647eafdab86c9355e35c44f7874f8d875
Ki-Tak <lee08tain@gmail.com>
2025-11-14 15:46:33 +0900

    feat(services): 데이터베이스 각 테이블 INSERT 문 작성

--- src/services/DatabaseService.js
+++ src/services/DatabaseService.js
@@ -54,6 +54,43 @@ class DatabaseService {
       CREATE INDEX IF NOT EXISTS idx_match_result_ticket_id ON match_result(ticket_id);
     `);
   }
-  // 추후, 필요한 기능 추가 예정
+
+  insertTicket(amount, ticketCount) {
+    const sql = "INSERT INTO lotto_ticket (amount,ticket_count) VALUES (?, ?)";
+    return this.#db.prepare(sql).run(amount, ticketCount).lastInsertRowid;
+  }
+
+  insertLotto(ticketId, lottoNumbers) {
+    const sql = "INSERT INTO lotto (ticket_id, numbers) VALUES (?, ?)";
+    lottoNumbers.forEach((numbers) => {
+      this.#db.prepare(sql).run(ticketId, JSON.stringify(numbers));
+    });
+  }
+
+  insertWinningNumbers(ticketId, numbers, bonusNumber) {
+    const sql =
+      "INSERT INTO winning_numbers (ticket_id, numbers, bonus_number) VALUES (?, ?, ?)";
+    return this.#db
+      .prepare(sql)
+      .run(ticketId, JSON.stringify(numbers), bonusNumber).lastInsertRowid;
+  }
+
+  insertMatchResult(ticketId, rankCount, totalPrize, returnRate) {
+    const sql = `INSERT INTO match_result 
+               (ticket_id, rank_1, rank_2, rank_3, rank_4, rank_5, total_prize, return_rate)
+               VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
+    this.#db
+      .prepare(sql)
+      .run(
+        ticketId,
+        rankCount[1],
+        rankCount[2],
+        rankCount[3],
+        rankCount[4],
+        rankCount[5],
+        totalPrize,
+        returnRate
+      );
+  }
 }
 export default DatabaseService;
