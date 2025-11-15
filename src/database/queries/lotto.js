d9459dd50c595bfab3dbbffd120fec25be3d4ead
Ki-Tak <lee08tain@gmail.com>
2025-11-15 11:21:12 +0900

    feat(database): 각 테이블 필요 SELECT 문 작성

--- src/database/queries/lotto.js
+++ src/database/queries/lotto.js
@@ -1,4 +1,5 @@
 const LOTTO_QUERY = {
   INSERT: "INSERT INTO lotto (ticket_id, numbers) VALUES (?, ?)",
+  SELECT_NUMBERS: "SELECT numbers FROM lotto WHERE ticket_id = ? ORDER BY id",
 };
 export default LOTTO_QUERY;
--- src/database/queries/winningNumbers.js
+++ src/database/queries/winningNumbers.js
@@ -1,5 +1,6 @@
 const WINNING_NUMBERS_QUERY = {
   INSERT:
     "INSERT INTO winning_numbers (ticket_id, numbers, bonus_number) VALUES (?, ?, ?)",
+  SELECT_ALL: "SELECT * FROM winning_numbers WHERE ticket_id = ?",
 };
 export default WINNING_NUMBERS_QUERY;
